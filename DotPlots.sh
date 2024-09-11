5cfdf95ce0bf0724aaf2398fc4e7e7b8d8035099
luanelandau <145043266+luanelandau@users.noreply.github.com>
2024-09-11 12:21:11 -0400

    Update DotPlots.sh

--- DotPlots.sh
+++ DotPlots.sh
@@ -1,64 +1,54 @@
-####I need to organize and comment this file!
-
-
-
-
-
-
-
 #!/bin/bash
 #SBATCH --account=omergokc
 #SBATCH --partition=general-compute
 #SBATCH --qos=nih
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
-#SBATCH --job-name="hg38_dot_HG01946_reads"
-#SBATCH --output=dot_HG01946_reads_hg38.out
-#SBATCH --error=dot_HG01946_reads_hg38.err
+#SBATCH --job-name="dothap1_HG01946_verkko_hg38"
+#SBATCH --output=dothap1_HG01946_verkko_hg38.out
+#SBATCH --error=dothap1_HG01946_verkko_hg38.err
 ##SBATCH --requeue
 #SBATCH --cpus-per-task=12
 #SBATCH --array=0-12 # job array index
 #Specifies that the job will be requeued after a node failure.
 #The default is that the job will not be requeued.
-echo "SLURM_JOBID="$SLURM_JOBID
-echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
-echo "SLURM_NNODES"=$SLURM_NNODES
-echo "SLURMTMPDIR="$SLURMTMPDIR
 
-echo "SLURM_JOBID="$SLURM_JOBID
-echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
-echo "SLURMTMPDIR="$SLURMTMPDIR
+####make sure the number of array matches the number of chromosomes.
+##Also, run the second part of this script separately, and delete the arrays. 
 
-echo "working directory = "$SLURM_SUBMIT_DIR
-TMPDIR=/tmp
-echo "TMPDIR="$TMPDIR
-
-ulimit -s unlimited
-
-#Ref="/projects/academic/omergokc/charikleia/t2t_chr.fa"
-Ref="/projects/academic/omergokc/hg38.fa"
-assembly="/projects/academic/omergokc/Luane/HG01946/nanoq-flye/results_nanoq_10kb_HG01946_ULR/HG01946_10kbreads.fastq"
-rn="hg38"
+######Defining references
+#Ref="/projects/academic/omergokc/charikleia/t2t_chr.fa"#T2T
+Ref="/projects/academic/omergokc/hg38.fa" #human hg38
+assembly="/projects/academic/omergokc/Luane/HG01946/verkko/diploid/HG01946_verkko_set5/assembly.haplotype1.fasta" #path to your assembly
+rn="hg38" #reference name, just for naming files
 #rn="t2t"
-kb="reads_"
-outdir="HG01946_hg38_reads_flye"
+outdir="HG01946_hg38_verkko" #name of the out directory
 
+#I am activating my conda environment, because this is where I have all the programs installed.
 eval "$(/projects/academic/omergokc/own_modules/2023.01/2023.01/software/Core/anaconda3/2023.03-1/bin/conda shell.bash hook)"
-
 conda activate Assembly
 
+#make outdirectory
 mkdir ${outdir}
 
-minimap2 -x asm5 ${Ref} ${assembly} > ${outdir}/${rn}_vs_reads_HG01946_ULR_${kb}kb.paf
+#loading the necessary modules
+module load gcc/11.2.0 openmpi/4.1.1 r/4.2.0
 
-#module load gcc/11.2.0
-#module load openmpi/4.1.1
-#module load r/4.2.0
+#mapping the assembly to the reference and generating a paf file
+minimap2 -x asm5 ${Ref} ${assembly} > ${outdir}/${rn}_vs_verkko_HG01946.paf
 
-#cut -f6 ${outdir}/${rn}_vs_reads_HG01946_ULR_${kb}kb.paf| sort -n | uniq | while read line; do grep $line ${outdir}/${rn}_vs_reads_HG01946_ULR_${kb}kb.paf > ${outdir}/${rn}_HG01946_${kb}kb_${line}.paf; /projects/academic/omergokc/Luane/softwares/paf2dotplot/paf2dotplot.r ${outdir}/${rn}_HG01946_${kb}kb_${line}.paf; done
+#this command cuts the sixth column of the paf file, that contains the chromosome to which it is aligned
+#then it sorts the names of the chromosomes and while reading each chromosome, greps the entire line and save it to a separate file
+#then automatically uses the paf2dotplot script on R to generate dotplots for the paf files that are separated by chromosome.
+cut -f6  ${outdir}/${rn}_vs_verkko_HG01946.paf| sort -n | uniq | while read line; do grep $line ${outdir}/${rn}_vs_verkko_HG01946.paf > ${outdir}/${rn}_HG01946_${line}.paf; /projects/academic/omergokc/Luane/softwares/paf2dotplot/paf2dotplot.r ${outdir}/${rn}_HG01946_${line}.paf; done
 
-/projects/academic/omergokc/Luane/softwares/paf2dotplot/paf2dotplot.r ${outdir}/${rn}_vs_reads_HG01946_ULR_${kb}kb.paf
+#this is just to also generate a dotplot for the file containing all the chromosomes.
+/projects/academic/omergokc/Luane/softwares/paf2dotplot/paf2dotplot.r ${outdir}/${rn}_vs_verkko_HG01946.paf
 
-conda deactivate
+#with this approach, chromosome 1 is not separated, because the command groups all chromosomes that start with 1. 
+#because this is my chromosome of interest, I am writing a command that only generates a paf and dotplot for chromosome 1.
+#only for chr1, because if not it'll be grouped with all the chr that start with 'chr1'
+awk -F'\t' '$6 == "chr1"' ${outdir}/${rn}_vs_verkko_HG01946.paf > ${outdir}/${rn}_HG01946_chr1_only.paf #The
+/projects/academic/omergokc/Luane/softwares/paf2dotplot/paf2dotplot.r ${outdir}/${rn}_HG01946_chr1_only.paf
